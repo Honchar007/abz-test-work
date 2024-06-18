060ac849eb7de73c16b7572bc72a75094a6a80fb
Andre <andrey2002335@gmail.com>
2024-06-18 16:58:46 +0300

    setup configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended', // Добавлено для интеграции с Prettier
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'prettier'], // Добавлен плагин Prettier
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true, // Принудительное добавление точек с запятой
+        singleQuote: true,
+        printWidth: 80,
+        'comma-dangle': [
+          'error',
+          {
+            arrays: 'always-multiline',
+            objects: 'always-multiline',
+            imports: 'always-multiline',
+            exports: 'always-multiline',
+            functions: 'ignore',
+          },
+        ],
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// styles
+import './assets/styles/index.scss';
+
+function App() {
+  return (
+    <>
+      <div>Hi</div>
+      <h1>Hi</h1>
+    </>
+  );
+}
+
+export default App;
